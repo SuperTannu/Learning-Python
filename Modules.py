f2373de2447545736140fd615ccb62f56212025f
SuperTannu <TejaswaniSharma3005@gmail.com>
2024-03-11 23:34:24 +0530

    Mod

--- Modules.py
+++ Modules.py
@@ -43,7 +43,49 @@ print(deq)
 
 import itertools
 
+#Product
 a=[1,2,3,4]
 b=[5,6,7]
 pro=itertools.product(a,b)
 print(list(pro))
+
+#Permutations and Combinations
+perm=itertools.permutations(a,2)
+combi=itertools.combinations(a,3)
+combiwr=itertools.combinations_with_replacement(a,2)
+print(list(perm))
+print(list(combi))
+print(list(combiwr))
+
+#Accumulate
+acc=itertools.accumulate(a)
+print(a)
+print(list(acc))
+
+import operator
+accu=itertools.accumulate(a, func=operator.mul)
+print(list(accu))
+
+#Groupby
+
+def lt(x):
+    return x<=3
+
+gr=itertools.groupby(a, key=lt)
+for key, value in gr:
+    print(key,list(value))
+
+#Infinite Operators
+for i in itertools.count(5): #Count
+    print(i)
+    if i>29:
+        break
+
+for i in itertools.cycle(a): #Cycle
+    print(i)
+    break
+
+
+rep=itertools.repeat(30,5) #Repeat
+print(list(rep))
+
