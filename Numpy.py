8c4baaeb8e1b539c8425789a67003551eaa19900
SuperTannu <TejaswaniSharma3005@gmail.com>
2024-03-28 18:46:11 +0530

    NumPy completed

--- Numpy.py
+++ Numpy.py
@@ -71,3 +71,28 @@ print(f'Sum of values in s: {s.sum()}')
 
 #Reorganizing
 print(f'Reshaped s: \n{s.reshape((3,2))}')
+print(f'Verically Stacked: \n{vstack([A,B])}')
+print(f'Horizontally Stacked: \n{hstack([A,B])}')
+
+#Miscellaneous
+da=genfromtxt('a.txt',delimiter=',').astype('int32')
+print(f'Data from a.txt: \n{da}')
+print(f'Boolean Data from a.txt: \n{da>29}')
+print(f'List Indexing array A: \n{A[[2,3,1,4]]}')
+print(f'Any function on array da: \n{any(da<31, axis=0)}')
+print(f'All function on array da: \n{all(da<31, axis=0)}')
+
+# Question
+qsnmatrix=array([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25],[26,27,28,29,30]])
+print(f'Question Matrix: \n{qsnmatrix}')
+print(f' Question 1=> Access 11,12,16 and 17')
+print(f' Question 2=> Access 2,8,14 and 20')
+print(f' Question 3=> Access 4,5,24,25,29 and 30')
+
+ans1=qsnmatrix[2:4,0:2]
+ans2=qsnmatrix[[0,1,2,3],[1,2,3,4]]
+ans3=qsnmatrix[[0,4,5],3:]
+
+print(f'Answer 1: \n{ans1}')
+print(f'Answer 2: \n{ans2}')
+print(f'Answer 3: \n{ans3}')
